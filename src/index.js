6c7b9b5957426095a8422f80079e29a73a5a71f7
yashraikar1087 <yashraikar1087@gmail.com>
2019-02-13 19:15:14 -0700

    feat(jsx) simple jsx

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import ReactDOM from "react-dom";
 
 // create a react component
 const App = function() {
-  return <div> Hi There</div>;
+  return (
+    <div style={{ backgroundColor: "red", color: "white" }}> Hi There</div>
+  );
 };
 // Take the react component and show it on the screen
 ReactDOM.render(<App />, document.querySelector("#root"));
